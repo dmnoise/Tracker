8cbb550b61bcd24af72c62024a20f92af42c8939
dmnoise <svaston@hotmail.com>
2025-06-06 13:23:13 +0300

    Добавил разделитель над таббаром

--- Tracker/TabBarController.swift
+++ Tracker/TabBarController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,5 +31,19 @@ final class TabBarController: UITabBarController {
         statsVC.tabBarItem = tabBatItemStats
         
         self.viewControllers = [trakcerVC, statsVC]
+        
+        setupBorder()
+    }
+    
+    private func setupBorder() {
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
+        tabBar.backgroundColor = .yaWhite
+        
+        tabBar.layer.shadowColor = UIColor.tabBarLine.cgColor
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: -0.5)
+        tabBar.layer.shadowOpacity = 1
+        tabBar.layer.shadowRadius = 0
+        tabBar.layer.masksToBounds = false
     }
 }
