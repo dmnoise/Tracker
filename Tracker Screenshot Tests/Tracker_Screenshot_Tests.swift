1ed0ef73bbdcbd69d76e79503bd40e920146bf1a
dmnoise <svaston@hotmail.com>
2025-07-21 12:53:19 +0300

    Добавил скриншот тест для главного экрана

---
+++ Tracker Screenshot Tests/Tracker_Screenshot_Tests.swift
@@ -0,0 +1,20 @@
+//
+//  Tracker_Screenshot_Tests.swift
+//  Tracker Screenshot Tests
+//
+//  Created by Dmitriy Noise on 21.07.2025.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class Tracker_Screenshot_Tests: XCTestCase {
+
+    func testViewController() {
+        let vc = TabBarController()
+        
+        assertSnapshot(of: vc, as: .image)
+    }
+
+}
